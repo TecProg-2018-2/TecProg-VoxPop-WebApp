cdca66e7da6b6ce40df53b03e8e68d7e9e5f9e88
Amanda Vieira Pires <amanda.pires@cabal.com.br>
2018-09-21 21:15:25 -0300

    Adding more comments

--- src/app/token.service.ts
+++ src/app/token.service.ts
@@ -1,35 +1,61 @@
+  /**********************************************************************
+  * File: token.service.ts
+  * Purpose: TokenService class implementation
+  * Notice: All rights reserved.
+  * Description File: Responsible for all session token operations
+  ***********************************************************************/
+
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 
+/**
+ * Responsible for all session token operations
+ */
 @Injectable()
 export class TokenService {
-
-  tokenValue: string = ''
-
+  
+  /**
+   * Creates an instance of token service.
+   * @param router Angular class that navigates other pages
+   * @param cookieService Service that operates on session cookies  
+   */
   constructor(
     private router: Router,
     private cookieService: CookieService,
   ) { }
 
-  getToken() {
-    this.tokenValue = this.cookieService.get('basic_token');
-    if(this.tokenValue != '') {
-      return 'Token ' + this.tokenValue;
+  /**
+   * Gets token from session cookie
+   * @returns token value
+   */
+  getToken(): string {
+    var tokenValue = this.cookieService.get('basic_token');
+
+    /* Checks whether the token has been stored in two session cookie variables */
+    if(tokenValue != '') {
+      return 'Token ' + tokenValue;
     }
     else {
-      this.tokenValue = this.cookieService.get('bearer_token');
-      if(this.tokenValue != '') {
-        return 'Bearer ' + this.tokenValue;
+      tokenValue = this.cookieService.get('bearer_token');
+      if(tokenValue != '') {
+        return 'Bearer ' + tokenValue;
       }
     }
     return '';
   }
 
-  checkToken(token: any) {
+  /**
+   * Checks token is valid
+   * @param token token value
+   * @returns true if token 
+   */
+  checkToken(token: any): boolean {
+    /* If the token is valid it disables some features and releases other */
     if (token === '') {
       return false;
     } else {
+      // TODO: Change the style from template.
       document.getElementById('register').style.display = 'none';
       document.getElementById('login').style.display = 'none';
       document.getElementById('deSuaOpiniao').style.display = 'block';
@@ -38,18 +64,31 @@ export class TokenService {
       return true;
     }
   }
-
-  filterRestrictPage(token: any) {
+  
+  /**
+   * Determines the route according to the token
+   * @param token token value
+   * @returns true if restrict page 
+   */
+  filterRestrictPage(token: any): boolean {
+    /* if there is no token it means that the user is not logged in */
     if (token === '') {
       this.router.navigate(['login']);
       return true;
-    }
+    } 
+    return false;
   }
-
-  filterLoginPage(token: any) {
+  
+  /**
+   * Filters login page
+   * @param token 
+   * @returns true if login page 
+   */
+  filterLoginPage(token: any): boolean {
     if (token !== '') {
       this.router.navigate(['']);
       return true;
     }
+    return false;
   }
 }
