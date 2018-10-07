a76349edc8b5055f99a5dc29865e27772b0afc8e
Djorkaeff Alexandre Vilela Pereira <djorkaeff.unb@gmail.com>
2018-10-07 17:55:19 -0300

    Add asserts on componentes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -33,10 +33,14 @@ export class LoginComponent implements OnInit {
     ngOnInit() {
         this.tokenValue = this.token.getToken();
         this.token.checkToken(this.tokenValue);
+
         this.assert.assert(this.tokenValue == null, 'Token vazio');
+
         this.token.filterLoginPage(this.tokenValue);
         this.registerSuccess = this.cookieService.get('success');
+
         this.assert.ok(this.registerSuccess != null);
+
         this.checkRegister(this.registerSuccess);
     }
 
@@ -59,13 +63,16 @@ export class LoginComponent implements OnInit {
         if (success === 'true') {
             const registerAlert: string = document.getElementById('registerAlert').style.display = 'block';
         }
+
         this.assert.ok(success=='true');
     }
 
     handleLoginResponse(request) {
         request.subscribe(response => {
             if (this.requester.didSucceed(response.status)) {
+
                 this.assert.ok(this.requester.didSucceed(response.status)!=null);
+
                 this.cookieService.set('basic_token', response.body['token']);
                 this.cookieService.set('userID', response.body['id']);
                 this.cookieService.set('userUsername', response.body['username']);
